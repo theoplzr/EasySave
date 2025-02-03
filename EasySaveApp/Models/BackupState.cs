6fa9064d70d38f806be9830d28d099a52874fc45
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:10:21 +0100

    add comment on BackupState

--- EasySaveApp/Models/BackupState.cs
+++ EasySaveApp/Models/BackupState.cs
@@ -6,17 +6,37 @@ namespace EasySaveApp.Models
 {
     public class BackupState
     {
+        // Name of the backup
         public required string BackupName { get; set; }
+        
+        // Time of the last action performed
         public DateTime LastActionTime { get; set; }
+        
+        // Current status of the backup
         public required string Status { get; set; } 
+        
+        // Total number of files to be backed up
         public int TotalFiles { get; set; }
+        
+        // Total size of files to be backed up
         public long TotalSize { get; set; }
+        
+        // Number of files remaining to be backed up
         public int RemainingFiles { get; set; }
+        
+        // Size of files remaining to be backed up
         public long RemainingSize { get; set; }
+        
+        // Current source file being backed up
         public required string CurrentSourceFile { get; set; }
+        
+        // Current target file being backed up
         public required string CurrentTargetFile { get; set; }
+        
+        // Unique identifier for the backup job
         public Guid JobId { get; set; }
 
+        // Method to save the state of backups to a file
         public static void SaveState(List<BackupState> states, string filePath)
         {
             File.WriteAllText(filePath, JsonConvert.SerializeObject(states, Formatting.Indented));
