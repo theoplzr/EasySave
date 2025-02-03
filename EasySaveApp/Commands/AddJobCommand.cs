7a76bb459cb6f2dda2923c1bad6d6370d68a07f7
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:18:56 +0100

    add comments for AddJobCommand

--- EasySaveApp/Commands/AddJobCommand.cs
+++ EasySaveApp/Commands/AddJobCommand.cs
@@ -4,14 +4,16 @@ namespace EasySaveApp.Commands
 {
     public class AddJobCommand : BackupCommand
     {
-        private BackupJob _jobToAdd;
+        private BackupJob _jobToAdd; // The backup job to be added
 
+        // Constructor to initialize the AddJobCommand with the backup manager and the job to add
         public AddJobCommand(BackupManager backupManager, BackupJob jobToAdd)
             : base(backupManager)
         {
             _jobToAdd = jobToAdd;
         }
 
+        // Method to execute the command to add the backup job
         public override void Execute()
         {
             _backupManager.AddBackupJob(_jobToAdd);
