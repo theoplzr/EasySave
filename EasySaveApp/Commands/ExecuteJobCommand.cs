800e752f4c7c6b2c8485c234e4d7990abe414ae2
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:19:40 +0100

    add comments for ExecuteJobCommand

--- EasySaveApp/Commands/ExecuteJobCommand.cs
+++ EasySaveApp/Commands/ExecuteJobCommand.cs
@@ -2,14 +2,16 @@ namespace EasySaveApp.Commands
 {
     public class ExecuteJobCommand : BackupCommand
     {
-        private int _jobIndex;
+        private int _jobIndex; // Index of the job to be executed
 
+        // Constructor to initialize the ExecuteJobCommand with the backup manager and job index
         public ExecuteJobCommand(BackupManager backupManager, int jobIndex)
             : base(backupManager)
         {
             _jobIndex = jobIndex;
         }
 
+        // Method to execute the command to run the specified backup job
         public override void Execute()
         {
             _backupManager.ExecuteBackupByIndex(_jobIndex);
