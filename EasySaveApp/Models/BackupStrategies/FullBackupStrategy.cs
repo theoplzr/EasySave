b69d2bb35e3cee7a1109b722e5bed432435040a2
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:14:04 +0100

    add coments for FullBackupStrategy

--- EasySaveApp/Models/BackupStrategies/FullBackupStrategy.cs
+++ EasySaveApp/Models/BackupStrategies/FullBackupStrategy.cs
@@ -1,12 +1,13 @@
-//classe qui permet de créer une stratégie de sauvegarde en fonction du type de sauvegarde
+// classe qui permet de créer une stratégie de sauvegarde en fonction du type de sauvegarde
 
 namespace EasySaveApp.Models.BackupStrategies
 {
     public class FullBackupStrategy : IBackupStrategy
     {
+        // Method to determine if a file should be copied
         public bool ShouldCopyFile(string sourceFilePath, string targetFilePath)
         {
-            return true; // Toujours copier les fichiers en sauvegarde complète
+            return true; // Always copy files in a full backup
         }
     }
 }
