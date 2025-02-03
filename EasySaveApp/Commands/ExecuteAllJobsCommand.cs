675428d3ea9462a2397802e27dd77bd027629abf
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:19:29 +0100

    add comments for ExecuteAllJobCommand

--- EasySaveApp/Commands/ExecuteAllJobsCommand.cs
+++ EasySaveApp/Commands/ExecuteAllJobsCommand.cs
@@ -2,11 +2,13 @@ namespace EasySaveApp.Commands
 {
     public class ExecuteAllJobsCommand : BackupCommand
     {
+        // Constructor to initialize the ExecuteAllJobsCommand with the backup manager
         public ExecuteAllJobsCommand(BackupManager backupManager)
             : base(backupManager)
         {
         }
 
+        // Method to execute the command to run all backup jobs
         public override void Execute()
         {
             _backupManager.ExecuteAllJobs();
