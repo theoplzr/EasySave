e541f642b14ce72df1f0598614775680992cb90d
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:20:16 +0100

    add comments for RemoveJobCommand

--- EasySaveApp/Commands/RemoveJobCommand.cs
+++ EasySaveApp/Commands/RemoveJobCommand.cs
@@ -7,14 +7,16 @@ namespace EasySaveApp.Commands
     /// </summary>
     public class RemoveJobCommand : BackupCommand
     {
-        private int _indexToRemove;
+        private int _indexToRemove; // Index of the job to be removed
 
+        // Constructor to initialize the RemoveJobCommand with the backup manager and index to remove
         public RemoveJobCommand(BackupManager backupManager, int indexToRemove)
             : base(backupManager)
         {
             _indexToRemove = indexToRemove;
         }
 
+        // Method to execute the command to remove the specified backup job
         public override void Execute()
         {
             _backupManager.RemoveBackupJob(_indexToRemove);
