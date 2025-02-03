2465dfce57caab4b18a17154491ebdc910746e92
rosier8u <basile.rosier6@etu.univ-lorraine>
2025-02-03 10:13:29 +0100

    add coments for DifferentialBackupStrategy

--- EasySaveApp/Models/BackupStrategies/DifferentialBackupStrategy.cs
+++ EasySaveApp/Models/BackupStrategies/DifferentialBackupStrategy.cs
@@ -1,16 +1,20 @@
-//classe qui permet de créer une stratégie de sauvegarde en fonction du type de sauvegarde
+// classe qui permet de créer une stratégie de sauvegarde en fonction du type de sauvegarde
 
 namespace EasySaveApp.Models.BackupStrategies
 {
     public class DifferentialBackupStrategy : IBackupStrategy
     {
+        // Method to determine if a file should be copied based on its last modified time
         public bool ShouldCopyFile(string sourceFilePath, string targetFilePath)
         {
+            // If the target file does not exist, the file should be copied
             if (!File.Exists(targetFilePath)) return true;
 
+            // Get the last modified time of the source and target files
             var sourceLastModified = File.GetLastWriteTime(sourceFilePath);
             var targetLastModified = File.GetLastWriteTime(targetFilePath);
 
+            // Copy the file if the source file is newer than the target file
             return sourceLastModified > targetLastModified;
         }
     }
