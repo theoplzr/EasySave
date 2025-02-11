f2620e7f4957ca061f56f08478ea7b61a36100c5
silba57 <basile.rosier57@gmail.com>
2025-02-11 17:23:56 +0100

    added comments

--- EasySave.GUI/ViewModels/ListAllJobViewModel.cs
+++ EasySave.GUI/ViewModels/ListAllJobViewModel.cs
@@ -9,22 +9,52 @@ using System;
 
 namespace EasySave.GUI.ViewModels
 {
+    /// <summary>
+    /// ViewModel for displaying and managing the list of all backup jobs.
+    /// This class handles listing, selecting, and removing backup jobs.
+    /// </summary>
     public class ListAllJobViewModel : ReactiveObject
     {
+        /// <summary>
+        /// Reference to the window associated with this ViewModel.
+        /// Used to close the window when needed.
+        /// </summary>
         private readonly Window? _window;
+        /// <summary>
+        /// Facade providing access to the backup jobs management logic.
+        /// </summary>
         private readonly EasySaveFacade _facade;
+        /// <summary>
+        /// The currently selected backup job in the UI.
+        /// </summary>
         private BackupJob? _selectedBackupJob;
+        /// <summary>
+        /// Collection of backup jobs to be displayed in the UI.
+        /// </summary>
         public ObservableCollection<BackupJob> BackupJobs { get; }
-
+        /// <summary>
+        /// Command to close the window.
+        /// </summary>
         public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+        /// <summary>
+        /// Command to remove the selected backup job.
+        /// </summary>
         public ReactiveCommand<Unit, Unit> RemoveCommand { get; }
-
+        /// <summary>
+        /// Gets or sets the currently selected backup job.
+        /// When a job is selected in the UI, this property is updated.
+        /// </summary>
         public BackupJob? SelectedBackupJob
         {
             get => _selectedBackupJob;
             set => this.RaiseAndSetIfChanged(ref _selectedBackupJob, value);
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ListAllJobViewModel"/> class.
+        /// </summary>
+        /// <param name="window">The window associated with this ViewModel.</param>
+        /// <param name="facade">The facade providing access to backup job operations.</param>
         public ListAllJobViewModel(Window window, EasySaveFacade facade)
         {
             _window = window;
@@ -35,11 +65,18 @@ namespace EasySave.GUI.ViewModels
             RemoveCommand = ReactiveCommand.Create(Remove, this.WhenAnyValue(x => x.SelectedBackupJob).Select(job => job != null));
         }
 
+        /// <summary>
+        /// Closes the window when the Cancel button is clicked.
+        /// </summary>
         private void Cancel()
         {
             _window?.Close();
         }
 
+        /// <summary>
+        /// Removes the selected backup job from the list.
+        /// If a job is selected, it finds the index and removes it from the facade and UI.
+        /// </summary>
         private void Remove()
         {
             if (SelectedBackupJob != null)
